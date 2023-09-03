a3661ad50f399300c3b537283b8ab70954401452
joonda <143615224+joonda@users.noreply.github.com>
2023-09-03 12:02:13 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,5 @@
+select user_id, nickname, concat(city, ' ', street_address1, ' ', street_address2) as '전체주소',
+concat(substr(TLNO, 1, 3),'-',substr(TLNO, 4, 4),'-',substr(TLNO,8,4)) as '전화번호' from used_goods_user
+where user_id in
+    (select writer_id from used_goods_board group by writer_id having count(*) >= 3)
+order by user_id desc
